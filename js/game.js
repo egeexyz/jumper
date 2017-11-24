14aed1540a380df0449ccfeae1d63fe62ff68820
egee-irl <brian@egee.io>
2017-11-23 20:12:57 -0800

    get rid of comments

--- js/game.js
+++ js/game.js
@@ -16,116 +16,67 @@ let score = 0
 let scoreText
 
 function create () {
-    //  We're going to be using physics, so enable the Arcade Physics system
   game.physics.startSystem(Phaser.Physics.ARCADE)
-
-    //  A simple background for our game
   game.add.sprite(0, 0, 'sky')
 
-    //  The platforms group contains the ground and the 2 ledges we can jump on
   platforms = game.add.group()
-
-    //  We will enable physics for any object that is created in this group
   platforms.enableBody = true
 
-    // Here we create the ground.
   let ground = platforms.create(0, game.world.height - 64, 'ground')
-
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
   ground.scale.setTo(2, 2)
-
-    //  This stops it from falling away when you jump on it
   ground.body.immovable = true
 
-    //  Now let's create two ledges
   let ledge = platforms.create(400, 450, 'ground')
   ledge.body.immovable = true
-
   ledge = platforms.create(-75, 350, 'ground')
   ledge.body.immovable = true
 
-    // The player and its settings
   player = game.add.sprite(32, game.world.height - 150, 'dude')
 
-    //  We need to enable physics on the player
   game.physics.arcade.enable(player)
 
-    //  Player physics properties. Give the little guy a slight bounce.
   player.body.bounce.y = 0.2
   player.body.gravity.y = 800
   player.body.collideWorldBounds = true
-
-    //  Our two animations, walking left and right.
   player.animations.add('left', [0, 1, 2, 3], 10, true)
   player.animations.add('right', [5, 6, 7, 8], 10, true)
 
-    //  Finally some stars to collect
   stars = game.add.group()
-
-    //  We will enable physics for any star that is created in this group
   stars.enableBody = true
 
-    //  Here we'll create 12 of them evenly spaced apart
   for (var i = 0; i < 12; i++) {
-        //  Create a star inside of the 'stars' group
     let star = stars.create(i * 70, 0, 'star')
-
-        //  Let gravity do its thing
     star.body.gravity.y = 1000
-
-        //  This just gives each star a slightly random bounce value
     star.body.bounce.y = 0.3 + Math.random() * 0.2
   }
-
-    //  The score
   scoreText = game.add.text(16, 16, '', { fontSize: '32px', fill: '#000' })
-
-    //  Our controls.
   cursors = game.input.keyboard.createCursorKeys()
 }
 
 function update () {
-    //  Collide the player and the stars with the platforms
   game.physics.arcade.collide(player, platforms)
   game.physics.arcade.collide(stars, platforms)
-
-    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
   game.physics.arcade.overlap(player, stars, collectStar, null, this)
 
-    //  Reset the players velocity (movement)
   player.body.velocity.x = 0
-
   if (cursors.left.isDown) {
-    //  Move to the left
     player.body.velocity.x = -150
-
     player.animations.play('left')
   } else if (cursors.right.isDown) {
-    //  Move to the right
     player.body.velocity.x = 150
-
     player.animations.play('right')
   } else {
-    //  Stand still
     player.animations.stop()
-
     player.frame = 4
   }
 
-    //  Allow the player to jump if they are touching the ground.
   if (cursors.up.isDown && player.body.touching.down) {
     player.body.velocity.y = -400
-  }
-  if (score === 120) {
-    process.crash()
   }
 }
 
 function collectStar (player, star) {
-    // Removes the star from the screen
   star.kill()
-
-    //  Add and update the score
   score += 10
   scoreText.text = 'Score: ' + score
 }

5f30d07e575f1d4bca51dc9afe991cd9a0972c9d
egee-irl <brian@egee.io>
2017-12-02 05:22:31 -0800

    updated comments

--- game.js
+++ game.js
@@ -3,8 +3,8 @@ const game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, crea
 function preload () {
   game.load.image('sky', 'assets/sky.png')
   game.load.image('ground', 'assets/platform.png')
-  game.load.image('star', 'assets/star.png')
-  game.load.spritesheet('dude', 'assets/dude.png', 32, 48)
+  game.load.image('diamond', 'assets/diamond.png')
+  game.load.spritesheet('woof', 'assets/woof.png', 32, 32)
 }
 let score = 0
 
@@ -12,7 +12,7 @@ let scoreText
 let platforms
 let cursors
 let player
-let stars
+let diamonds
 
 
 function create () {
@@ -45,7 +45,7 @@ function create () {
   ledge.body.immovable = true
 
     // The player and its settings
-  player = game.add.sprite(32, game.world.height - 150, 'dude')
+  player = game.add.sprite(32, game.world.height - 150, 'woof')
 
     //  We need to enable physics on the player
   game.physics.arcade.enable(player)
@@ -56,76 +56,72 @@ function create () {
   player.body.collideWorldBounds = true
 
     //  Our two animations, walking left and right.
-  player.animations.add('left', [0, 1, 2, 3], 10, true)
-  player.animations.add('right', [5, 6, 7, 8], 10, true)
+  player.animations.add('left', [0, 1], 10, true)
+  player.animations.add('right', [2, 3], 10, true)
 
-    //  Finally some stars to collect
-  stars = game.add.group()
+    //  Finally some diamonds to collect
+  diamonds = game.add.group()
 
-    //  We will enable physics for any star that is created in this group
-  stars.enableBody = true
+    //  Enable physics for any object that is created in this group
+  diamonds.enableBody = true
 
-    //  Here we'll create 12 of them evenly spaced apart
+    //  Create 12 diamonds evenly spaced apart
   for (var i = 0; i < 12; i++) {
-        //  Create a star inside of the 'stars' group
-    let star = stars.create(i * 70, 0, 'star')
+    let diamond = diamonds.create(i * 70, 0, 'diamond')
 
-        //  Let gravity do its thing
-    star.body.gravity.y = 1000
-
-        //  This just gives each star a slightly random bounce value
-    star.body.bounce.y = 0.3 + Math.random() * 0.2
+      //  Drop em from the sky and make them bounce a bit
+    diamond.body.gravity.y = 1000
+    diamond.body.bounce.y = 0.3 + Math.random() * 0.2
   }
 
-    //  The score
+    //  Create the score text
   scoreText = game.add.text(16, 16, '', { fontSize: '32px', fill: '#000' })
 
-    //  Our controls.
+    //  And bootstrap our controls
   cursors = game.input.keyboard.createCursorKeys()
 }
 
 function update () {
-    //  Collide the player and the stars with the platforms
-  game.physics.arcade.collide(player, platforms)
-  game.physics.arcade.collide(stars, platforms)
 
-    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-  game.physics.arcade.overlap(player, stars, collectStar, null, this)
-
-    //  Reset the players velocity (movement)
+    //  We want the player to stop when not moving
   player.body.velocity.x = 0
 
+    //  Setup collisions for the player, diamonds, and our platforms
+  game.physics.arcade.collide(player, platforms)
+  game.physics.arcade.collide(diamonds, platforms)
+
+    //  Call callectionDiamond() if player overlaps with a diamond
+  game.physics.arcade.overlap(player, diamonds, collectDiamond, null, this)
+
+    // Configure the controls!
   if (cursors.left.isDown) {
-    //  Move to the left
     player.body.velocity.x = -150
 
     player.animations.play('left')
   } else if (cursors.right.isDown) {
-    //  Move to the right
     player.body.velocity.x = 150
 
     player.animations.play('right')
   } else {
-    //  Stand still
+    // If no movement keys are pressed, stop the player
     player.animations.stop()
-
-    player.frame = 4
   }
 
-    //  Allow the player to jump if they are touching the ground.
+    //  This allows the player to jump!
   if (cursors.up.isDown && player.body.touching.down) {
     player.body.velocity.y = -400
   }
+    // You win!
   if (score === 120) {
-    process.crash()
+    // alert('You win!')
   }
 }
 
-function collectStar (player, star) {
-    // Removes the star from the screen
-  star.kill()
+function collectDiamond (player, diamond) {
+    // Removes the diamond from the screen
+  diamond.kill()
 
-    //  Add and update the score
+    //  And update the score
   score += 10
   scoreText.text = 'Score: ' + score
 }

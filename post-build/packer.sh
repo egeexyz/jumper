a47503aaee7eefd0a43d63708f987708c15ee0df
egee-irl <brian@egee.io>
2018-08-21 22:23:21 -0700

    builds

---
+++ post-build/packer.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+wget https://releases.hashicorp.com/packer/1.2.5/packer_1.2.5_linux_amd64.zip
+unzip packer_1.2.5_linux_amd64.zip
+
+./packer validate post-build/jumper.json
+pip install --user awscli
+export PATH=$PATH:$HOME/.local/bin
+eval $(aws ecr get-login --no-include-email --region us-west-2)
+./packer build post-build/jumper.json

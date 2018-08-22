754e81044c6ab916c56ed8e58aae867d29851064
egee-irl <brian@egee.io>
2018-08-21 22:23:10 -0700

    fixed assets

--- public/game.js
+++ public/game.js
@@ -14,10 +14,10 @@ let player
 
 function preload () {
   // Load & Define our game assets
-  game.load.image('sky', '../assets/sky.png')
-  game.load.image('ground', '../assets/platform.png')
-  game.load.image('diamond', '../assets/diamond.png')
-  game.load.spritesheet('woof', '../assets/woof.png', 32, 32)
+  game.load.image('sky', 'sky.png')
+  game.load.image('ground', 'platform.png')
+  game.load.image('diamond', 'diamond.png')
+  game.load.spritesheet('woof', 'woof.png', 32, 32)
 }
 
 function create () {

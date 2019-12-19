37aee406b6f51697801a998bc6390e29480d69c7
egee-irl <brian@egee.io>
2019-12-19 00:02:57 -0800

    commented

--- post-build/packer.sh
+++ post-build/packer.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
+# I wrote this post-build deploy stuff for a job interview challenge.
+# It's not used anymore but the code is pretty nifty so I'm keeping it for posterity.
+
 set -e
 
 wget https://releases.hashicorp.com/packer/1.2.5/packer_1.2.5_linux_amd64.zip

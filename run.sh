d7e4e9004cd4f50c1ea257a909e687b34c8a9934
egeexyz <brian@egee.io>
2025-06-23 17:48:42 -0700

    turn into lil cli

--- run.sh
+++ run.sh
@@ -3,5 +3,19 @@
 ## Runs Jumper as a service in docker. ##
 ## Connect via http://127.0.0.1:8080   ##
 #########################################
-set -eux
-docker run -it -p 8080:8080 ghcr.io/egeexyz/jumper:latest
+operation=$1
+repo=${2:-ghcr.io/egeexyz/jumper}
+version=${3:-latest}
+
+if [[ $operation = -b ]]; then
+    docker build -t "ghcr.io/egeexyz/jumper:$version" .
+elif [[ $operation = -p ]]; then
+    docker push "ghcr.io/egeexyz/jumper:$version"
+elif [[ $operation = -r ]]; then
+    docker run -it -p 8080:8080 "$repo"
+else
+    echo "available options:
+    -b: build a new container image
+    -p: push container image to image repository
+    -r: run jumper as a service"
+fi
